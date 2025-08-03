a8d4842d70adc493f815a24551dd9471b1dbfda8
Liam Gilbride <liam@gilbride.ca>
2025-08-03 13:25:41 -0700

    basic arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// action - can be one of list, add, remove, complete
+    action: String,
+
+    /// object - what to do action to
+    object: u32,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:#?}", args);
 }
